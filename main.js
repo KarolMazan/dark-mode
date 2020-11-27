0fc0a60377abfaee41a68f1181240d523fcf2017
Karol Mazan <mazankarol@gmail.com>
2020-11-27 14:16:46 +0100

    dark mode

---
+++ main.js
@@ -0,0 +1,15 @@
+let isDark = false
+
+const switchMode = document.querySelector('.mode--js')
+
+switchModes = addEventListener('click', () => {
+    if (isDark) {
+        document.documentElement.style.setProperty('--background-color', 'rgb(241, 236, 236)')
+        document.documentElement.style.setProperty('--text-color', 'rgb(14, 13, 13)')
+        isDark = false;
+    } else {
+        document.documentElement.style.setProperty('--background-color', 'rgb(14, 13, 13)')
+        document.documentElement.style.setProperty('--text-color', 'rgb(241, 236, 236)')
+        isDark = true;
+    }
+})
